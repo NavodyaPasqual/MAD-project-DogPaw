eacbe17e467972b5450269a7d7a9bd00e813df5f
Kushani <kushanikavindya44@gmail.com>
2020-08-16 15:17:33 +0530

    MainActiviy page added MainActivity2 page added MainActivity3 page added MainActivity4 page added MainActivity5 page added MapsActivity page added MapsActivity2 page added MapsActivity3 page added MapsActivity4 page added

--- app/src/main/java/com/example/dogpaw/MainActivity.java
+++ app/src/main/java/com/example/dogpaw/MainActivity.java
@@ -8,9 +8,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Slide;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toolbar;
 
 import java.util.ArrayList;
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private List<Slide> slideList = new ArrayList<>();
     private ViewPager pager;
     private PagerAdapter adaptor;
+    private Button button;
 
 
     @Override
@@ -30,21 +34,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mDrawerlayout = (DrawerLayout) findViewById(R.id.drawer);
-        mTroggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open,R.string.close);
-        mTroggle = new ActionBarDrawerToggle(this,mDrawerlayout,R.string.open,R.string.close);
+        mTroggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
+        mTroggle = new ActionBarDrawerToggle(this, mDrawerlayout, R.string.open, R.string.close);
         mDrawerlayout.addDrawerListener(mTroggle);
         mTroggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-
+        button = (Button) findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity2();
+            }
+        });
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(mTroggle.onOptionsItemSelected(item)){
+        if (mTroggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void openMainActivity2() {
+        Intent intent = new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
