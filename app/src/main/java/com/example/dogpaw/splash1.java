2adc048ac4f2a551fdf45cac609f6514e1fc1ae7
Kushani <kushanikavindya44@gmail.com>
2020-10-01 00:37:38 +0530

    individual trial2

---
+++ app/src/main/java/com/example/dogpaw/splash1.java
@@ -0,0 +1,39 @@
+package com.example.dogpaw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splash1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splasher_screen);
+
+        Thread thread =  new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(6000);
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent welcomeIntent = new Intent(splash1.this,MainActivity.class);
+                    startActivity(welcomeIntent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        finish();
+    }
+}
